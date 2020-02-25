c64dfd0e00498b2f0da079b1cdb45ef0fcdf5933
Gilgamesh <zhousy@smips.cn>
2020-02-25 16:23:13 +0800

    更新死锁

--- src/MyThreadPoolDemo.java
+++ src/MyThreadPoolDemo.java
@@ -1,10 +1,39 @@
 // 多线程中，第三种获得多线程的方式
 // 多一条注释
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+
+import java.util.concurrent.*;
 
 public class MyThreadPoolDemo {
     public static void main(String[] args) {
+        ExecutorService threadPool = new ThreadPoolExecutor(
+                2,
+                5,
+                1L,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<Runnable>(3),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy());
+                // 当任务数超过 maximumPoolSize 和等待队列数量之和就报错
+         try {
+            for (int i = 0; i < 10; i++) {
+                threadPool.execute(() -> {
+                    System.out.println(Thread.currentThread().getName() + "办理业务");
+                });
+                //暂停一会
+//                try {
+//                    TimeUnit.SECONDS.sleep(1);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            threadPool.shutdown();
+        }
+    }
+
+    private static void SimpleThread() {
         // 底层用 LinkedBlockingQueue
 //        ExecutorService threadPool = Executors.newFixedThreadPool(5);
 //        ExecutorService threadPool = Executors.newSingleThreadExecutor();
